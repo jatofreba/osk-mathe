7f229ae7eb471f3534007835a788818133bf85f3
freddie <jherzig@posteo.net>
2026-06-23 18:13:30 +0200

    fix: deploy.sh Umlaute entfernt fuer cron-Kompatibilitaet

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ export PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin"
 APP_DIR="/opt/matheherz"
 LOG="/var/log/matheherz-deploy.log"
 
-echo "[$(date '+%Y-%m-%d %H:%M:%S')] Cron läuft..." >> "$LOG"
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] Cron running..." >> "$LOG"
 
 cd "$APP_DIR" || { echo "[$(date '+%Y-%m-%d %H:%M:%S')] FEHLER: cd $APP_DIR fehlgeschlagen" >> "$LOG"; exit 1; }
 
@@ -15,9 +15,9 @@ REMOTE=$(git rev-parse origin/master)
 echo "[$(date '+%Y-%m-%d %H:%M:%S')] LOCAL=$LOCAL REMOTE=$REMOTE" >> "$LOG"
 
 if [ "$LOCAL" != "$REMOTE" ]; then
-  echo "[$(date '+%Y-%m-%d %H:%M:%S')] Neuer Commit erkannt – deploye..." >> "$LOG"
+  echo "[$(date '+%Y-%m-%d %H:%M:%S')] Neuer Commit - deploying..." >> "$LOG"
   git reset --hard origin/master >> "$LOG" 2>&1
   npm install --quiet >> "$LOG" 2>&1
   pm2 restart matheherz >> "$LOG" 2>&1
-  echo "[$(date '+%Y-%m-%d %H:%M:%S')] Deploy abgeschlossen." >> "$LOG"
+  echo "[$(date '+%Y-%m-%d %H:%M:%S')] Deploy done." >> "$LOG"
 fi
